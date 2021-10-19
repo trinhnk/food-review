d57e877eddb70467f429cafc376215ddcdce917b
trinhnk <nguyenkimtrinh1993@gmail.com>
2021-10-20 00:48:32 +0700

    add model post

--- food_review/settings.py
+++ food_review/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'food_review.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'food_review',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- review/views.py
+++ review/views.py
@@ -1,12 +1,15 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'review/home.html')
+    posts = Post.objects.all()
+    return render(request, 'review/home.html', {'posts': posts})
 
 def post(request):
+    # post = Post.objects.get()
     return render(request, 'review/post.html')
 
 def create(request):
